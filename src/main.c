df2c87f084f45b6263a7ea0f438d72e32a62ef01
Rohan St Hill <rohan.sthill@gmail.com>
2021-06-21 10:08:37 +1000

    making more memory efficient

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@ typedef struct coord
     double **u;
 } coord;
 
+// consier writing a function that can allocate and free the memory for my struct
 
 coord block2tower(coord foam, double towerDist, int len);
 
@@ -34,7 +35,7 @@ int main(int argc, char *argv[]){
 coord block2tower(coord foam, double towerDist, int len){
     // defining variables
     int i;
-    double *tx, *tu, uTower, xTower;
+    double tx, tu, uTower, xTower;
     coord towerCoords;
 
     uTower = -towerDist/2.0;
@@ -44,31 +45,25 @@ coord block2tower(coord foam, double towerDist, int len){
     towerCoords.u = malloc(sizeof(towerCoords.u)*len);
     towerCoords.x = malloc(sizeof(towerCoords.x)*len);
 
-    tu = malloc(sizeof(tu)*len);
-    tx = malloc(sizeof(tx)*len);
 
     // calculating values
     for(i=0;i<len;i++){
-        tu[i] = (xTower - foam.x[i][0])/(foam.u[i][0] - foam.x[i][0]);
-        tu[i] = (uTower - foam.x[i][0])/(foam.u[i][0] - foam.x[i][0]);
-    }
+        tu = (xTower - foam.x[i][0])/(foam.u[i][0] - foam.x[i][0]);
+        tx = (uTower - foam.x[i][0])/(foam.u[i][0] - foam.x[i][0]);
 
-    for(i=0;i<len;i++){
         towerCoords.x[i][0] = xTower;
         towerCoords.u[i][0] = uTower;
 
-        towerCoords.u[i][1] = foam.x[i][0] + tu[i]*(foam.u[i][1]-foam.x[i][1]);
-        towerCoords.u[i][2] = foam.x[i][2] + tu[i]*(foam.u[i][2]-foam.x[i][2]);
+        towerCoords.u[i][1] = foam.x[i][0] + tu*(foam.u[i][1]-foam.x[i][1]);
+        towerCoords.u[i][2] = foam.x[i][2] + tu*(foam.u[i][2]-foam.x[i][2]);
 
-        towerCoords.x[i][1] = foam.x[i][0] + tx[i]*(foam.u[i][1]-foam.x[i][1]);
-        towerCoords.x[i][2] = foam.x[i][2] + tx[i]*(foam.u[i][2]-foam.x[i][2]);
+        towerCoords.x[i][1] = foam.x[i][0] + tx*(foam.u[i][1]-foam.x[i][1]);
+        towerCoords.x[i][2] = foam.x[i][2] + tx*(foam.u[i][2]-foam.x[i][2]);
     }
 
     // freeing memory
     free(foam.x);
     free(foam.u);
-    free(tx);
-    free(tu);
     
     return towerCoords;
 
